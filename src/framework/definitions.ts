85ae51f4002856c0908c3ad7451f8450e36f5570
Lucas Stranks <me@lucastranks.com>
2025-12-08 12:42:46 -0800

    fix: update type for framework command accepted data to support slash command options

--- src/framework/definitions.ts
+++ src/framework/definitions.ts
@@ -1,28 +1,34 @@
 // Resources
 import type {
-    ChatInputCommandInteraction,
-    Client,
-    SlashCommandBuilder,
-    SlashCommandSubcommandsOnlyBuilder,
-    Events, Interaction
+  ChatInputCommandInteraction,
+  Client,
+  SlashCommandBuilder,
+  SlashCommandSubcommandsOnlyBuilder,
+  Events,
+  Interaction,
+  SlashCommandOptionsOnlyBuilder,
 } from "discord.js";
 
-/**
- * A file extension enforcing you to prefix a string with '.'.
- */
+/** A file extension enforcing you to prefix a string with '.'. */
 export type Extension = `.${string}`;
 
+/** The framework client base. Adds an additional map for commands storage. */
 export interface FrameworkClient extends Client {
-    commands: Map<string, unknown>;
+  commands: Map<string, unknown>;
 }
 
+/** Represents a command structure for the framework. */
 export interface FrameworkCommand {
-    data: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder;
-    execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
+  data:
+    | SlashCommandBuilder
+    | SlashCommandSubcommandsOnlyBuilder
+    | SlashCommandOptionsOnlyBuilder;
+  execute: (interaction: ChatInputCommandInteraction) => Promise<void>;
 }
 
+/** Represents an event structure for the framework. */
 export interface FrameworkEvent<DataType = Interaction> {
-    event: Events;
-    once?: boolean;
-    execute: (data: DataType) => Promise<void>;
+  event: Events;
+  once?: boolean;
+  execute: (data: DataType) => Promise<void>;
 }
